6499ad06093ab3ab45260119b0ed974b9e425402
Scott Doxey <neogeek0101@gmail.com>
2016-03-26 21:06:47 -0400

    Fixed path.

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@ define(function (require) {
 
     var game = require('./game/game'),
         intro = require('./game/intro'),
-        level = require('./game/level')('../../data/level1.json');
+        level = require('./game/level')('../data/level1.json');
 
     game.context.imageSmoothingEnabled = false;
 
